2901dc6eb205c34ffad4fbeef93d60706ee19dbb
alfonso contreras <alfonso.contreras@alegra.com>
2024-10-26 00:55:14 -0500

    agregando las validaciones del dto para bodega y obteniendo el producto

--- bodega-ms/src/bodega/bodega.controller.ts
+++ bodega-ms/src/bodega/bodega.controller.ts
@@ -1,8 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { BodegaService } from './bodega.service';
+import { RetrieveIngredientsDto  } from './dto';
 
 @Controller('bodega')
 export class BodegaController {
   constructor(private readonly bodegaService: BodegaService) {}
+  /**
+   * permite obtener los ingredientes de la bodega
+   */
+  @Get('/retrieve-ingredient')
+  retrieveIngredients(@Query() ingredient: RetrieveIngredientsDto) {
+    return this.bodegaService.retrieveIngredients(ingredient)
+  }
 }
 
--- bodega-ms/src/bodega/bodega.service.ts
+++ bodega-ms/src/bodega/bodega.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { RetrieveIngredientsDto } from './dto';
+import { IngredientEntity } from './entity/ingredient.entity';
+import { IngredientName } from './enum/ingredients-name.enum';
 
 @Injectable()
-export class BodegaService {}
+export class BodegaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  private async findIngredient(ingredientName:string){
+    try{
+      const ingredient = await this.ingredient.findUnique({
+        where: {
+          name: ingredientName,
+        }
+      })
+      
+      if (!ingredient){
+        throw new NotFoundException(`ingredient not found`)
+      }
+
+      const { id, name, quantity } = ingredient
+      const nameEnum = IngredientName[name as keyof typeof IngredientName];
+      return new IngredientEntity(id, nameEnum , quantity)
+    }catch(error) {
+      console.error(error)
+    }
+    
+  }
+  
+  async retrieveIngredients({ ingredientName, quantity}: RetrieveIngredientsDto){
+      const ingredient = await this.findIngredient(ingredientName);
+
+      if ( ingredient.getQuantity() - quantity < 0 ) {
+        // TODO: comprar los ingredientes a la plaza
+        return;
+      }
+      
+      await ingredient.UpdateQuantity(quantity)
+      
+      return ingredient.toApiJSON()
+  }
+
+
+}
