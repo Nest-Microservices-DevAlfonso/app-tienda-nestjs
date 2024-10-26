e124f9cc02a41525588ca7aca8fd570be50c4fc6
alfonso contreras <alfonso.contreras@alegra.com>
2024-10-25 23:12:07 -0500

    creacion de seeders en micro bodega y configuraciones varias

--- bodega-ms/src/main.ts
+++ bodega-ms/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import envs from './config/envs';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  const logger = new Logger("Bodega-ms | Main")
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.NATS,
+  //     options: {
+  //       servers: envs.NATS_SERVERS
+  //     }
+  //   }
+  // );
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(envs.PORT);
+  logger.log("Microservicio Bodega-ms Corriendo")
 }
 bootstrap();
